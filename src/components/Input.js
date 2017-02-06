7c1e3091961822c67bb07633608add6924b8e039
Danny Kim <dkim7474@gmail.com>
2017-02-06 16:17:43 -0500

    Removed unneeded imports for now

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,36 +1,5 @@
 import React from 'react';
-import Errors from './Errors';
-
-class List extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {toDo: this.props.toDo};
-        this.handleDelete = this.handleDelete.bind(this);
-    }
-    handleDelete(e) {
-        console.log(e.target.value);
-        let deleteArray = this.props.toDo;
-        let removedArr = deleteArray.splice(e.target.value, 1);
-        this.setState({
-            toDo: removedArr
-        })
-        console.log(this.state.toDo);
-    }
-    render() {
-        const listArr = this.props.toDo;
-        const listItem = listArr.map((item, index) =>
-                <li key={index} value={item}>
-                    {item}
-                    <button onClick={this.handleDelete} value={index}>Delete</button>
-                </li>
-            )
-        return (
-            <div>
-                {listItem}
-            </div>
-        )
-    }
-}
+import List from './List';
 
 class ToDoForm extends React.Component {
     constructor(props) {
@@ -71,7 +40,6 @@ class ToDoForm extends React.Component {
                         value="Submit"
                     />
                 </form>
-                <Errors value={this.state.value} />
             </div>
         )
     }
--- src/components/List.js
+++ src/components/List.js
@@ -1,1 +1,34 @@
+import React from 'react';
 
+class List extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {toDo: this.props.toDo};
+        this.handleDelete = this.handleDelete.bind(this);
+    }
+    handleDelete(e) {
+        console.log(e.target.value);
+        let deleteArray = this.props.toDo;
+        let removedArr = deleteArray.splice(e.target.value, 1);
+        this.setState({
+            toDo: removedArr
+        })
+        console.log(this.state.toDo);
+    }
+    render() {
+        const listArr = this.props.toDo;
+        const listItem = listArr.map((item, index) =>
+                <li key={index} value={item}>
+                    {item}
+                    <button onClick={this.handleDelete} value={index}>Delete</button>
+                </li>
+            )
+        return (
+            <div>
+                {listItem}
+            </div>
+        )
+    }
+}
+
+export default List;
