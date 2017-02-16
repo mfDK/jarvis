2320f647f0980bad13243bf06b938db4a2c51beb
Danny Kim <dkim7474@gmail.com>
2017-02-16 15:36:03 -0500

    Adding Nav component with react-router - branch

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ToDoForm from './components/Input';
 import Motivation from './components/Motivation/Motivation';
+import Navigation from './components/Nav/Nav';
 import 'whatwg-fetch';
 import style from './Index.css'
 
 function App() {
     return (
         <div>
+            <Navigation />
             <div className={style.toDo} >
                 <ToDoForm />
             </div>
