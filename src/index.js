39dd555a22f7e33813d4e8fa469b21601d6674ba
Danny Kim <dkim7474@gmail.com>
2017-02-20 12:00:29 -0500

    Created index route so / routes does not show blank

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, hashHistory, Link } from 'react-router';
+import { Router, Route, hashHistory, Link, IndexRoute } from 'react-router';
 import ToDoForm from './components/TaskList/Input';
 import Motivation from './components/Motivation/Motivation';
+import Home from './components/Home/Home';
 import 'whatwg-fetch';
 import style from './Index.css';
 
@@ -13,10 +14,10 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <h1>Route Navigator</h1>
-                <ul role="nav">
-                    <li><Link to="/todolist" activeStyle={{ color: 'green'}} className={style.nav}>Task List</Link></li>
-                    <li><Link to="/motivation" activeStyle={{ color: 'green'}} className={style.nav}>Motivation</Link></li>
+                <h1>Jarvis</h1>
+                <ul role="nav" className={style.nav}>
+                    <li><Link to="/todolist" activeStyle={{ color: 'green'}} className={style.navItem}>Task List</Link></li>
+                    <li><Link to="/motivation" activeStyle={{ color: 'green'}} className={style.navItem}>Motivation</Link></li>
                 </ul>
                 {this.props.children}
             </div>
@@ -29,6 +30,7 @@ class App extends React.Component {
 ReactDOM.render(
     <Router history={hashHistory}>
         <Route path="/" component={App}>
+        <IndexRoute component={Home} />
             <Route path="/todolist" component={ToDoForm} />
             <Route path="/motivation" component={Motivation} />
         </Route>
